c5dfc319f691b6549e2976da8db3d86d1866e391
dangminhtruong <xdangminhtruongx@gmail.com>
2018-03-12 16:45:22 +0700

    Updates

---
+++ js/origin.js
@@ -0,0 +1,20 @@
+$(document).ready(function () { $(".memenu").memenu(); });
+
+$(document).ready(function () {
+    $('.popup-with-zoom-anim').magnificPopup({
+        type: 'inline',
+        fixedContentPos: false,
+        fixedBgPos: true,
+        overflowY: 'auto',
+        closeBtnInside: true,
+        preloader: false,
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'my-mfp-zoom-in'
+    });
+
+});
+
+
+
+
